9d23b750f796970c3e43f953e0c6c4e143a8d518
blackJJW <jjinwoo92@gmail.com>
2022-12-03 00:45:59 +0900

    20221203

--- c++_prac/ex19.cpp
+++ c++_prac/ex19.cpp
@@ -4,6 +4,8 @@
 int main() {
 	int choice;
 
+	makeChoice:
+
 	printf("새 게임 : 1\n");
 	printf("불러오기 : 2\n");
 	printf("설정 : 3\n");
@@ -11,5 +13,24 @@ int main() {
 
 	scanf("%d", &choice);
 
-	
+	switch(choice){
+		case 1:
+			printf("새 게임.\n");
+			break;
+		case 2:
+			printf("불러오기.\n");
+			break;
+		case 3:
+			printf("설정.\n");
+			break;
+		case 4:
+			printf("크레딧.\n");
+			break;
+		default:
+			printf("잘못 입력했습니다.\n");
+			goto makeChoice;
+			break;
+	}
+
+	printf("***\n");
 }
